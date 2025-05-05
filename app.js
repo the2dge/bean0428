9f2c162c2380628747c006cf0912ad9dc5ab4a51
the2dge <the2dge@users.noreply.github.com>
2025-05-05 09:47:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -735,7 +735,6 @@ if (lineUserName) {
 
     paymentMethodSelect.addEventListener('change', (e) => {
     if (e.target.value === 'credit-point') {
-        await setupCreditPointValidation();
         discountCodeWrapper.style.display = 'block';
         creditProofWrapper.style.display = 'none';
     } else if (e.target.value === 'credit-card') {
