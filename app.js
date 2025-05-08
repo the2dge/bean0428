31988273113f246ddcf5c69e4dd6ec33d55006b9
the2dge <the2dge@users.noreply.github.com>
2025-05-08 12:50:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -555,7 +555,7 @@ function renderCheckoutPage(cartItems, storeInfo = null) {
     <input type="email" id="email" name="email"  value="${storedEmail}" required>
 
     <label for="telephone">電話:</label>
-    <input type="tel" id="telephone" name="telephone" pattern="\d{10}" inputmode="numeric" required>
+    <input type="tel" id="telephone" name="telephone" pattern="[0-9]{10}" required>
     
 
     <label for="discount-code-wrapper" style="background:#eee">使用折扣碼?</label>
