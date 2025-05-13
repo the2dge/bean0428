66a9451e00d9ba32eeb5fe55ddf411d41c26e369
the2dge <the2dge@users.noreply.github.com>
2025-05-13 13:34:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1129,7 +1129,7 @@ console.log("Order Data for Submission to GAS (New Structure):", JSON.stringify(
             name: nameInput.value,
             // MerchantTradeDate: Formatted YYYY/MM/DD HH:MM:SS (Server should generate this ideally)
             totalAmount: sessionStorage.getItem('finalOrderAmountForSubmission') || 0,
-            pickupOption: "便利商店",
+            customField1: "便利商店",
             tradeDesc: 'Order Description', // Replace with your order description
             itemName: cart, // Replace with your product name
             returnUrl: 'https://asia-east1-ecpay-rtnmessage.cloudfunctions.net/handleECPayPost', // Replace with your ReturnURL
